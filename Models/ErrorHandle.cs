bfc6d0284703cc3180fdbe9cac4838e6812fbcbc
dasgshyam <dasgshyam@gmail.com>
2025-01-01 08:20:32 +0530

    Log writter exception issue fixed

--- Models/ErrorHandle.cs
+++ Models/ErrorHandle.cs
@@ -3,33 +3,40 @@ using System.IO;
 
 namespace HtmlGrabber.Models
 {
-    class ErrorHandle
+    internal class ErrorHandle
     {
-        private static object locker = new object();
+        private static readonly object locker = new object();
+
         public void WriteToLogFile(string message)
         {
             lock (locker)
             {
-                string docPath = AppDomain.CurrentDomain.BaseDirectory + "Log";
+                string docPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
                 int min = DateTime.Now.Minute;
                 string fileName;
-                string formate;
+                string format;
+
                 if (min % 5 == 0)
                 {
-                    formate = "dd-MM-yyyy HH-mm";
+                    format = "dd-MM-yyyy HH-mm";
                 }
                 else
                 {
                     int old_min = min - (min % 5);
-                    formate = "dd-MM-yyyy HH-" + old_min.ToString();
+                    format = "dd-MM-yyyy HH-" + old_min.ToString("D2");
                 }
-                fileName = "Log_" + DateTime.Now.ToString(formate) + ".txt";
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, fileName)))
+
+                fileName = "Log_" + DateTime.Now.ToString(format) + ".txt";
+                string filePath = Path.Combine(docPath, fileName);
+
+                if (!Directory.Exists(docPath))
                 {
-                    outputFile.Write(DateTime.Now.ToString() + "\t" + message + "\n");
+                    Directory.CreateDirectory(docPath);
                 }
+
+                using StreamWriter outputFile = new(filePath, append: true);
+                outputFile.WriteLine($"{DateTime.Now}\t{message}");
             }
         }
-
     }
 }
